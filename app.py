4ee382a721774f15edada54565df1716c573b7ef
abdmuzaffar <210068@akfauniversity.org>
2024-12-04 15:49:33 +0500

    Add docstrings to fix pylint warnings

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+"""
+This is a simple Python application that greets the user.
+"""
+
 def greet(name):
+    """
+    Returns a greeting message for the given name.
+
+    Args:
+        name (str): The name of the person to greet.
+
+    Returns:
+        str: A greeting message.
+    """
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
